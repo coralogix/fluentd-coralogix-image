de5d2d81df79d8f32283fdc88e72f7b60292118f
Eldar Aliiev <bc.zim.green@gmail.com>
2019-10-25 01:11:33 +0300

    Proxy support added

--- fluentd-coralogix-image/plugins/out_coralogix.rb
+++ fluentd-coralogix-image/plugins/out_coralogix.rb
@@ -13,31 +13,36 @@ module Fluent
     # First, register the plugin. NAME is the name of this plugin
     # and identifies the plugin in the configuration file.
     Fluent::Plugin.register_output('coralogix', self)
-    config_param :config, :hash, :default => {}
-    config_param :log_key_name, :string, :default => nil
+    config_param :config, :hash, :default => {}, deprecated: "This parameter is deprecated"
     config_param :privatekey, :string, :default => nil
     config_param :appname, :string, :default => ""
     config_param :subsystemname, :string, :default => ""
-    config_param :is_json, :bool, :default => false
+    config_param :log_key_name, :string, :default => nil
     config_param :timestamp_key_name, :default => nil
+    config_param :is_json, :bool, :default => false
     config_param :force_compression, :bool, :default => false
     config_param :debug, :bool, :default => false
+    config_section :proxy, :param_name => "proxy", required: false, multi: false do
+      config_param :host, :string
+      config_param :port, :integer
+      config_param :user, :string, :default =>  nil
+      config_param :password, :string, :default =>  nil, secret: true
+    end
     @configured = false
-
 
     # This method is called before starting.
     def configure(conf)
       super
       begin
         @loggers = {}
-       
+
         #If config parameters doesn't start with $ then we can configure Coralogix logger now.
         if !appname.start_with?("$") && !subsystemname.start_with?("$")
+          private_key = get_private_key
           app_name = config.fetch("APP_NAME", appname)
           sub_name = config.fetch("SUB_SYSTEM", subsystemname)
-          private_key = get_private_key
-          
-          @logger = CoralogixLogger.new private_key, app_name, sub_name, debug, "FluentD", force_compression
+
+          @logger = CoralogixLogger.new private_key, app_name, sub_name, debug, "FluentD (#{version?})", force_compression, proxy ? proxy.to_h.map { |k,v| [k.to_s,v] }.to_h : Hash.new
           @configured = true
         end
       rescue Exception => e
@@ -49,12 +54,20 @@ module Fluent
       return config.fetch("PRIVATE_KEY", privatekey)
     end
 
+    def version?
+      begin
+        Gem.loaded_specs['fluent-plugin-coralogix'].version.to_s
+      rescue Exception => e
+        return '0.0.0'
+      end
+    end
+
     def extract record, key, default
       begin
         res = record
         return key unless key.start_with?("$")
         key[1..-1].split(".").each do |k|
-          res = res.fetch(k,nil)
+          res = res.fetch(k, nil)
           return default if res == nil
         end
         return res
@@ -74,12 +87,12 @@ module Fluent
     def get_logger(record)
 
       return @logger if @configured
-      
+
       private_key = get_private_key
       app_name, sub_name = get_app_sub_name(record)
-      
+
       if !@loggers.key?("#{app_name}.#{sub_name}")
-        @loggers["#{app_name}.#{sub_name}"] = CoralogixLogger.new private_key, app_name, sub_name
+        @loggers["#{app_name}.#{sub_name}"] = CoralogixLogger.new private_key, app_name, sub_name, debug, "FluentD (#{version?})", force_compression, proxy ? proxy.to_h.map { |k, v| [k.to_s, v] }.to_h : Hash.new
       end
 
       return @loggers["#{app_name}.#{sub_name}"]
@@ -105,7 +118,7 @@ module Fluent
     # NOTE! This method is called by Fluentd's main thread so you should not write slow routine here. It causes Fluentd's performance degression.
     def emit(tag, es, chain)
       chain.next
-      es.each {|time,record|
+      es.each { |time, record|
         logger = get_logger(record)
 
         log_record = log_key_name != nil ? record.fetch(log_key_name, record) : record
@@ -113,15 +126,15 @@ module Fluent
         log_record = log_record.to_s.empty? ? record : log_record
 
         timestamp = record.fetch(timestamp_key_name, nil)
-        if(timestamp.nil?)
+        if (timestamp.nil?)
           logger.debug log_record
         else
           begin
             float_timestamp = DateTime.parse(timestamp.to_s).to_time.to_f * 1000
-            logger.debug log_record, nil, timestamp:float_timestamp
-          rescue Exception => e  
+            logger.debug log_record, nil, timestamp: float_timestamp
+          rescue Exception => e
             logger.debug log_record
-          end          
+          end
         end
       }
     end
